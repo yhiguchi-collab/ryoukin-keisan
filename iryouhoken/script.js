bb0b93a59d975443e86c1c7d84a16129e3e96afb
yhiguchi-collab <y.higuchi@totonoihoukan.jp>
2026-06-22 16:14:56 +0900

    複数名訪問看護加算（同一建物2人まで/3人以上 × 一般/精神）を追加

--- iryouhoken/script.js
+++ iryouhoken/script.js
@@ -88,6 +88,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const taiou24El = document.querySelector('input[name="taiou24"]:checked');
     if (taiou24El && taiou24El.value !== 'nashi') totalCostFull += IRYOU_KASAN.taiou24;
 
+    totalCostFull += counts.fukusuumei_nin2_ippan * IRYOU_KASAN.fukusuumei.nin2_ippan;
+    totalCostFull += counts.fukusuumei_nin2_seishin * IRYOU_KASAN.fukusuumei.nin2_seishin;
+    totalCostFull += counts.fukusuumei_nin3_ippan * IRYOU_KASAN.fukusuumei.nin3_ippan;
+    totalCostFull += counts.fukusuumei_nin3_seishin * IRYOU_KASAN.fukusuumei.nin3_seishin;
+
     if (taiinKyodoEl.checked) totalCostFull += IRYOU_KASAN.taiin_kyodo;
     if (taiinShienEl.checked) totalCostFull += IRYOU_KASAN.taiin_shien;
 
